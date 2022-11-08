39305390925ef1a19d9ae6171b98e6a546f923e5
Yash Karandikar <yash@karx.xyz>
2022-11-07 18:25:38 -0600

    tuning pain and suffering

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -33,7 +33,7 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = true;
+    public static final boolean RUN_USING_ENCODER = false;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
             getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
 
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 1;
+    public static double kA = 0.00017;
+    public static double kStatic = 0.01834;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -87,7 +87,7 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = 39.4224324932042;
+    public static double MAX_VEL = 27;
     public static double MAX_ACCEL = 39.4224324932042;
     public static double MAX_ANG_VEL = Math.toRadians(141.1711875);
     public static double MAX_ANG_ACCEL = Math.toRadians(141.1711875);
