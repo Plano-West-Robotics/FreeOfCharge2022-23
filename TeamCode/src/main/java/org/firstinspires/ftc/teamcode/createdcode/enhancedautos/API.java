3171c89202f14c65b33ac18aa7ff3cbe12b028a5
Yash Karandikar <yash@karx.xyz>
2022-11-08 12:53:53 -0600

    Remove unused from API

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/createdcode/enhancedautos/API.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/createdcode/enhancedautos/API.java
@@ -1,12 +1,6 @@
 package org.firstinspires.ftc.teamcode.createdcode.enhancedautos;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 public class API {
     OpMode opMode;
@@ -20,18 +14,6 @@ public class API {
         while (true) {
             if (!(opMode.getRuntime() < time)) break; // stop android studio from yelling
         }
-    }
-
-
-    public Trajectory makeTrajectories(SampleMecanumDrive mecanumDrive, Pose2d startPos, int[] x, int[] y) {
-        assert x.length == y.length;
-        TrajectoryBuilder builder = mecanumDrive.trajectoryBuilder(startPos);
-
-        for (int i = 0; i < x.length; i++) {
-            builder.splineTo(new Vector2d(x[i], y[i]), 0);
-        }
-
-        return builder.build();
     }
 
     public int getLargest(int x, int y, int z) {
