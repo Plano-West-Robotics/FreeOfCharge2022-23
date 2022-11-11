1e48b51aad3b02659669e1b2c5a81eaaeedaf650
Yash Karandikar <yash@karx.xyz>
2022-11-10 20:03:33 -0600

    new new lift system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/createdcode/teleops/DriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/createdcode/teleops/DriveTeleOp.java
@@ -10,7 +10,8 @@ public class DriveTeleOp extends OpMode {
     //Drive Variables
     private DcMotor motorFR, motorFL, motorRR, motorRL;
     private double powerFR, powerFL, powerRR, powerRL;
-    private DcMotor motorLift;
+    private DcMotor motorLiftLeft;
+    private DcMotor motorLiftRight;
     private double powerLift;
     private double drive = 0, strafe = 0, turn = 0;
     private double speed = 1;
@@ -29,7 +30,7 @@ public class DriveTeleOp extends OpMode {
         drive = -1 * gamepad1.left_stick_y;
         strafe = gamepad1.left_stick_x;
         turn = gamepad1.right_stick_x;
-        powerLift = gamepad2.left_stick_y;
+        powerLift = -1 * gamepad2.left_stick_y;
         if (gamepad1.left_bumper && last_left_bumper != gamepad1.left_bumper) speed = Math.max(0, speed - 0.15);
         if (gamepad1.right_bumper && last_right_bumper != gamepad1.right_bumper) speed = Math.min(1, speed + 0.15);
 
@@ -57,7 +58,8 @@ public class DriveTeleOp extends OpMode {
         motorRR.setPower(powerRR);
         motorRL.setPower(powerRL);
 
-        motorLift.setPower(powerLift);
+        motorLiftLeft.setPower(powerLift);
+        motorLiftRight.setPower(powerLift);
 
         telemetry.addData("speed", String.format("%.2f", speed));
         telemetry.update();
@@ -78,28 +80,27 @@ public class DriveTeleOp extends OpMode {
         motorRR = hardwareMap.get(DcMotor.class, "rearRight");
         motorRL = hardwareMap.get(DcMotor.class, "rearLeft");
 
-        motorLift = hardwareMap.get(DcMotor.class, "lift");
+        motorLiftLeft = hardwareMap.get(DcMotor.class, "liftLeft");
+        motorLiftRight = hardwareMap.get(DcMotor.class, "liftRight");
 
         motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
         motorRL.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorLift.setDirection(DcMotorSimple.Direction.REVERSE);
 
         motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorRR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorRL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorRR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorRL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         motorFR.setPower(0);
         motorFL.setPower(0);
         motorRR.setPower(0);
         motorRL.setPower(0);
-        motorLift.setPower(0);
+        motorLiftLeft.setPower(0);
+        motorLiftRight.setPower(0);
     }
 }
