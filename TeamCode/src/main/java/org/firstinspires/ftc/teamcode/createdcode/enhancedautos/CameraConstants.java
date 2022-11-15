6d1582c26a6798faca164479bd40f6899ca27388
Yash Karandikar <yash@karx.xyz>
2022-11-14 18:28:32 -0600

    Camera auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/createdcode/enhancedautos/CameraConstants.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.createdcode.enhancedautos;
+
+public class CameraConstants {
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    public static final double FX = 578.272;
+    public static final double FY = 578.272;
+    public static final double CX = 402.145;
+    public static final double CY = 221.506;
+
+    public static final double TAGSIZE = 0.166;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/createdcode/enhancedautos/CameraTest.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.createdcode.enhancedautos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.createdcode.enhancedautos.vendor.AprilTagDetectionPipeline;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+@Autonomous
+public class CameraTest extends LinearOpMode {
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    // meters
+
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(
+                CameraConstants.TAGSIZE,
+                CameraConstants.FX,
+                CameraConstants.FY,
+                CameraConstants.CX,
+                CameraConstants.CY);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if (currentDetections.size() != 0) {
+                for (AprilTagDetection tag : currentDetections) {
+                    telemetry.addLine(String.format("Detected tag ID=%d", tag.id));
+                }
+                telemetry.update();
+            } else {
+                telemetry.addLine("No tag detected");
+                telemetry.update();
+            }
+        }
+
+    }
+}
