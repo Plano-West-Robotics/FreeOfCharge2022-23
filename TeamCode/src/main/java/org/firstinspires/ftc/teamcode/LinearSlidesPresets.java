7afc1a3aac8d0033ac33c650ba3fd1e2ba064163
Hemanthsai Peddasani <hpeddasani7@hemanthsais-macbook-pro.local>
2022-12-02 09:05:14 -0600

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlidesPresets.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous
+public class LinearSlidesPresets extends LinearOpMode {
+
+    //inches
+    public static final double CLAW_TO_GROUND = 3;
+    public static final double SMALL_JUNCTIONS = 13.3;
+    public static final double MEDIUM_JUNCTIONS = 23.1;
+    public static final double TALL_JUNCTIONS = 33.2;
+    public static final double CIRCUMFERENCE = 4.40945;
+    public static final double TICKS_PER_REV = 537.7;
+
+    //motor: 5203 Series Yellow GoBilda motor
+    // RPM: 223
+    // spool = 4.40945
+
+
+
+
+    //Ticks per revolutions: 537.7
+    // num of inches / circum  * ticks
+
+
+
+   double targetPosSmall =  (SMALL_JUNCTIONS/CIRCUMFERENCE) * TICKS_PER_REV;
+   double targetPosMedium =  (MEDIUM_JUNCTIONS/CIRCUMFERENCE) * TICKS_PER_REV;
+   double targetPosTall =  (TALL_JUNCTIONS/CIRCUMFERENCE) * TICKS_PER_REV;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor liftMotor = hardwareMap.get(DcMotor.class, "slide");
+        waitForStart();
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        while (opModeIsActive()) {
+            telemetry.addData("Ticks: ", liftMotor.getCurrentPosition());
+            telemetry.update();
+
+            liftMotor.setTargetPosition((int) targetPosSmall);
+            liftMotor.setPower(0.5);
+
+            while (liftMotor.isBusy()) {
+
+            }
+        }
+    }
+
+
+    //switch case
+
+
+
+
+
+
+}
