b0673802381e173a2b175970a3aa83199ca38ed7
Yash Karandikar <yash@karx.xyz>
2022-12-19 19:02:57 -0600

    Add InchWorm tester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/InchTest.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(group = "test")
+public class InchTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        InchWorm inchWorm = new InchWorm(this);
+
+        waitForStart();
+
+        inchWorm.drive(24);
+        inchWorm.strafe(24);
+    }
+}
