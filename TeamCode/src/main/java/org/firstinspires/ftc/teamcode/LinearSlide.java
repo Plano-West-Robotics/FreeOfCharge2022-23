eab763849353f5758171dbb14647447419ed6007
Yash Karandikar <yash@karx.xyz>
2023-03-17 18:58:39 -0500

    Add docs to LinearSlide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlide.java
@@ -20,27 +20,51 @@ public class LinearSlide {
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
+    /**
+     * Set the target position of the lift.
+     * @param pos position to go to.
+     */
     public void setTargetPosition(int pos) {
         motor.setTargetPosition(pos);
     }
 
+    /**
+     * Set the target position of the lift.
+     * @param pos position to go to.
+     */
     public void setTargetPosition(SlidePresets pos) {
         setTargetPosition(pos.position); // lmao
     }
 
+    /**
+     * Checks if the motor is busy
+     * @return Whether the motor is running towards a certain position AND the opMode is still active.
+     */
     public boolean isBusy() {
         return motor.isBusy() && opMode.opModeIsActive();
     }
 
+    /**
+     * Continually run towards a position.
+     * @param pos position to run to.
+     */
     public void runToPosition(int pos) {
         setTargetPosition(pos);
         while (isBusy()) {}
     }
 
+    /**
+     * Continually run towards a position.
+     * @param pos position to run to.
+     */
     public void runToPosition(SlidePresets pos) {
         runToPosition(pos.position);
     }
 
+    /**
+     * Set the power of the motor.
+     * @param power New power
+     */
     public void setPower(double power) {
         motor.setPower(power);
     }
